1b6c54e94103ed1e9dc2a2009cefa71b5a911957
Matthias Stephan <evolvingapps@gmail.com>
2022-07-23 16:06:26 +0200

    added looping over plugins and themes

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs)
 docker-compose run --user 33 --rm wpcli wp core install --url=http://localhost:${WP_PORT} --title=test --admin_user=admin --admin_email=test@example.com
-docker-compose run --user 33 --rm wpcli wp plugin install duplicator --activate
+for name in $(cat plugins.txt); do 
+    docker-compose run --user 33 --rm wpcli wp plugin install $name --activate
+done
+for name in $(cat themes.txt); do 
+    docker-compose run --user 33 --rm wpcli wp theme install $name --activate
+done
