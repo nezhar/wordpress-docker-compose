ddd22181c8a8e1074d170ac6c8d14f5d44b1409d
Matthias Stephan <evolvingapps@gmail.com>
2022-07-24 19:47:37 +0200

    added wp variables

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs)
-docker-compose run --user 33 --rm wpcli wp core install --url=http://localhost:${WP_PORT} --title=test --admin_user=admin --admin_email=test@example.com
+docker-compose run --user 33 --rm wpcli wp core install --url=http://localhost:${WP_PORT} --title=${WP_TITLE} --admin_user=admin --admin_email=${WP_EMAIL}
 for name in $(cat plugins.txt); do 
     docker-compose run --user 33 --rm wpcli wp plugin install $name --activate
 done
